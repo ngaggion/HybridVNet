5b8d44a47121eb6f0a7626d8aa18a9274bdd3c15
ngaggion <ngaggion@gmail.com>
2024-08-23 14:28:26 -0300

    reorganize

--- inference_example.py
+++ inference_example.py
@@ -1,118 +1,14 @@
 import json
 import os
-import pickle as pkl
-import numpy as np
 import torch
 from models.hybridGNet_3D import HybridGNet3D
 from models.hybridGNet_3D_noLAX import HybridGNet3D as HybridGNet3D_noLAX
-from models.utils import scipy_to_torch_sparse
 from torchvision import transforms
 import SimpleITK as sitk
 import meshio 
 
-# Import the PredictDataset and transformation classes
 from utils.dataset_inference_w_LAX_example import PredictDataset, ToTorchTensors, PadArraysToSquareShape
-from utils.inference_aux import get_mask_image
-
-# Some auxiliars to avoid installing psbody-mesh during test
-class PartialUnpickler(pkl.Unpickler):
-    def find_class(self, module, name):
-        try:
-            return super().find_class(module, name)
-        except:
-            return FakeClass
-class FakeClass:
-    def __init__(self, *args, **kwargs):
-        pass
-def partial_load(file):
-    return PartialUnpickler(file).load()
-
-def configure_model(config):
-    matrix_path = config['matrix_path']
- 
-    with open(matrix_path, "rb") as f:
-        dic = partial_load(f)
-    
-    gpu = "cuda:" + str(config["cuda_device"])
-    device = torch.device(gpu if torch.cuda.is_available() else "cpu")
-    config['device'] = device
-
-    M, A, D, U = dic["M"], dic["A"], dic["D"], dic["U"]
-
-    D_t = [scipy_to_torch_sparse(d).to(device).float() for d in D]
-    U_t = [scipy_to_torch_sparse(u).to(device).float() for u in U]
-    A_t = [scipy_to_torch_sparse(a).to(device).float() for a in A]
-    num_nodes = [len(M[i].v) for i in range(len(M))]
-    
-    config['n_nodes'] = num_nodes
-
-    skip_connections = [True] * config['n_skips'] + [False] * (4 - config['n_skips']) if config['do_skip'] else [False] * 4
-
-    if "noLAX" in config['name']:      
-        model = HybridGNet3D_noLAX(config, D_t, U_t, A_t, skip_connections).float().to(device)
-    else:
-        model = HybridGNet3D(config, D_t, U_t, A_t, skip_connections).float().to(device)
-
-    return model, M[0].f
-
-
-def go_back(config, image, mesh_v, x0=0, y0=0):
-    # Scales and translates mesh vertices back to original image space
-    def get_both_paddings(desired, actual):
-        pad = desired - actual
-        v1, v2 = pad // 2, pad // 2
-        if v1 + v2 < pad:
-            v2 += 1
-        return v1, v2
-
-    # Get the origin of the image
-    origin = np.array(image.GetOrigin())
-    spacing = np.array(image.GetSpacing())
-    direction = image.GetDirection()
-
-    # Calculate the pixel size in each dimension
-    pixel_size = np.array([spacing[0], spacing[1], spacing[2]])
-    
-    outh, outw = config['h'], config['w']
-    
-    image = sitk.GetArrayFromImage(image)
-    original_h, original_w, z = min(image.shape[1], outh), min(image.shape[2],outw), min(image.shape[0], 16)
-
-    if config['full']:
-        # We have to remove the padding           
-        x_pad = get_both_paddings(outw, original_w)[0]
-        y_pad = get_both_paddings(outh, original_h)[0]
-        z_pad = get_both_paddings(16, z)[0]
-
-        # Transform to pixel coordinates
-        mesh_v[:, 0] = mesh_v[:, 0] * outw - x_pad
-        mesh_v[:, 1] = mesh_v[:, 1] * outh - y_pad
-        mesh_v[:, 2] = mesh_v[:, 2] * 16 - z_pad
-        mesh_v[:, 2] = z - mesh_v[:, 2] # Invert z axis
-
-        # Transform to physical coordinates
-        mesh_v[:, 0] *= pixel_size[0]
-        mesh_v[:, 1] *= pixel_size[1]
-        mesh_v[:, 2] *= pixel_size[2]
-
-        # Apply direction matrix
-        direction_matrix = np.array(direction).reshape(3, 3)
-        mesh_v = np.dot(mesh_v, direction_matrix.T)
-
-        # Add origin
-        mesh_v[:, 0] += origin[0]
-        mesh_v[:, 1] += origin[1]
-        mesh_v[:, 2] += origin[2]
-
-    return mesh_v
-
-def save_nii(filename, data, origin, spacing, direction):
-    dataITK = sitk.GetImageFromArray(data) 
-    dataITK.SetSpacing(spacing)
-    dataITK.SetOrigin(origin)
-    dataITK.SetDirection(direction)
-    sitk.WriteImage(dataITK, filename)
-
+from utils.inference_aux import configure_model, go_back, save_nii, get_mask_image
 
 def predict_meshes(config, model, faces, predict_dataset, meshes_path):
     model.eval()
@@ -149,7 +45,7 @@ def predict_meshes(config, model, faces, predict_dataset, meshes_path):
             )
 
             # Save as STL
-            path = meshes_path + str(subject) + "/" 
+            path = meshes_path + "/" + str(subject) + "/" 
             os.makedirs(path, exist_ok=True)
             path += str(time).replace('.nii.gz',".stl")
             
@@ -159,13 +55,7 @@ def predict_meshes(config, model, faces, predict_dataset, meshes_path):
             mask_seg = get_mask_image(mesh_vertices, faces, subpart_list, sax_image).transpose(2, 0, 1)
             save_nii(path.replace(".stl", ".nii.gz"), mask_seg, sax_image.GetOrigin(), sax_image.GetSpacing(), sax_image.GetDirection())
 
-
 if __name__ == "__main__":
-    class Mesh:
-        def __init__(self, v, f):
-            self.v = v
-            self.f = f
-
     output_dir = "../Abdul/Predictions"
     os.makedirs(output_dir, exist_ok=True)
 
--- utils/inference_aux.py
+++ utils/inference_aux.py
@@ -1,18 +1,105 @@
 import os 
-from utils.file_utils import load_folder
-import pandas as pd
+import pickle as pkl
 import numpy as np
-from trimesh import Trimesh
+import torch
+from models.utils import scipy_to_torch_sparse
 import SimpleITK as sitk
+from trimesh import Trimesh
 
 VOXEL_SIZE = 1.0
 
-def extract_subpart(points, faces, ids, subpartID):
-    # points: points
-    # faces: faces
-    # ids: IDs to extract
-    # subpartID: list of IDs for every point
+class PartialUnpickler(pkl.Unpickler):
+    def find_class(self, module, name):
+        try:
+            return super().find_class(module, name)
+        except:
+            return FakeClass
 
+class FakeClass:
+    def __init__(self, *args, **kwargs):
+        pass
+
+def partial_load(file):
+    return PartialUnpickler(file).load()
+
+def configure_model(config):
+    matrix_path = config['matrix_path']
+ 
+    with open(matrix_path, "rb") as f:
+        dic = partial_load(f)
+    
+    gpu = "cuda:" + str(config["cuda_device"])
+    device = torch.device(gpu if torch.cuda.is_available() else "cpu")
+    config['device'] = device
+
+    M, A, D, U = dic["M"], dic["A"], dic["D"], dic["U"]
+
+    D_t = [scipy_to_torch_sparse(d).to(device).float() for d in D]
+    U_t = [scipy_to_torch_sparse(u).to(device).float() for u in U]
+    A_t = [scipy_to_torch_sparse(a).to(device).float() for a in A]
+    num_nodes = [len(M[i].v) for i in range(len(M))]
+    
+    config['n_nodes'] = num_nodes
+
+    skip_connections = [True] * config['n_skips'] + [False] * (4 - config['n_skips']) if config['do_skip'] else [False] * 4
+
+    if "noLAX" in config['name']:      
+        model = HybridGNet3D_noLAX(config, D_t, U_t, A_t, skip_connections).float().to(device)
+    else:
+        model = HybridGNet3D(config, D_t, U_t, A_t, skip_connections).float().to(device)
+
+    return model, M[0].f
+
+def go_back(config, image, mesh_v, x0=0, y0=0):
+    def get_both_paddings(desired, actual):
+        pad = desired - actual
+        v1, v2 = pad // 2, pad // 2
+        if v1 + v2 < pad:
+            v2 += 1
+        return v1, v2
+
+    origin = np.array(image.GetOrigin())
+    spacing = np.array(image.GetSpacing())
+    direction = image.GetDirection()
+
+    pixel_size = np.array([spacing[0], spacing[1], spacing[2]])
+    
+    outh, outw = config['h'], config['w']
+    
+    image = sitk.GetArrayFromImage(image)
+    original_h, original_w, z = min(image.shape[1], outh), min(image.shape[2],outw), min(image.shape[0], 16)
+
+    if config['full']:
+        x_pad = get_both_paddings(outw, original_w)[0]
+        y_pad = get_both_paddings(outh, original_h)[0]
+        z_pad = get_both_paddings(16, z)[0]
+
+        mesh_v[:, 0] = mesh_v[:, 0] * outw - x_pad
+        mesh_v[:, 1] = mesh_v[:, 1] * outh - y_pad
+        mesh_v[:, 2] = mesh_v[:, 2] * 16 - z_pad
+        mesh_v[:, 2] = z - mesh_v[:, 2] # Invert z axis
+
+        mesh_v[:, 0] *= pixel_size[0]
+        mesh_v[:, 1] *= pixel_size[1]
+        mesh_v[:, 2] *= pixel_size[2]
+
+        direction_matrix = np.array(direction).reshape(3, 3)
+        mesh_v = np.dot(mesh_v, direction_matrix.T)
+
+        mesh_v[:, 0] += origin[0]
+        mesh_v[:, 1] += origin[1]
+        mesh_v[:, 2] += origin[2]
+
+    return mesh_v
+
+def save_nii(filename, data, origin, spacing, direction):
+    dataITK = sitk.GetImageFromArray(data) 
+    dataITK.SetSpacing(spacing)
+    dataITK.SetOrigin(origin)
+    dataITK.SetDirection(direction)
+    sitk.WriteImage(dataITK, filename)
+
+def extract_subpart(points, faces, ids, subpartID):
     ids = [ids] if not isinstance(ids, list) else ids
     
     n_points = len(points)
@@ -59,32 +146,24 @@ def get_grid(label, faces, subs):
     return full_mesh, grilla
 
 def assign_voxels(grid, mesh, value, full):
-        """
-        Internal method. Don't use it directly.
-        """
-        # get the _grid and indices where voxels are true
-        xs, ys, zs = np.meshgrid(range(mesh.shape[0]), range(mesh.shape[1]), range(mesh.shape[2]), indexing='ij')
-        xs = xs[mesh.matrix]
-        ys = ys[mesh.matrix]
-        zs = zs[mesh.matrix]
+    xs, ys, zs = np.meshgrid(range(mesh.shape[0]), range(mesh.shape[1]), range(mesh.shape[2]), indexing='ij')
+    xs = xs[mesh.matrix]
+    ys = ys[mesh.matrix]
+    zs = zs[mesh.matrix]
 
-        # convert those indices into point coordinates
-        mesh_pts = mesh.indices_to_points(np.stack([xs, ys, zs]).transpose())
+    mesh_pts = mesh.indices_to_points(np.stack([xs, ys, zs]).transpose())
 
-        # using main voxel_grid to get the indices inside rvlv
-        fhm_idx = full.points_to_indices(mesh_pts)
+    fhm_idx = full.points_to_indices(mesh_pts)
 
-        # assign value
-        grid[fhm_idx[:, 0], fhm_idx[:, 1], fhm_idx[:, 2]] = value
+    grid[fhm_idx[:, 0], fhm_idx[:, 1], fhm_idx[:, 2]] = value
 
-        return grid
+    return grid
 
 def get_tmesh(v, f, subpartID, subpart_ids):
     v, f = extract_subpart(v.copy(), f, subpartID, subpart_ids)
     return Trimesh(vertices=v, faces=f).voxelized(VOXEL_SIZE).fill()
 
 def get_mask_values(fhm, grilla, pos, outbound=0):
-    # get valid indices
     indices = fhm.points_to_indices(pos)
 
     j = (0 <= indices[:, 0]) & (indices[:, 0] < grilla.shape[0]) & \
@@ -95,7 +174,6 @@ def get_mask_values(fhm, grilla, pos, outbound=0):
     values[j] = grilla[indices[j, 0], indices[j, 1], indices[j, 2]]
 
     return values
-
 
 def get_mask_image(mesh, faces, subs, sax):
     width, height, num_slices = sax.GetSize()
